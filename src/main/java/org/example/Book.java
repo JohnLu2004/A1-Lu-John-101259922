8856add37e3a04c881c0d4888f4faca3f50b5cda
JohnLu2004 <john.lu10212004@gmail.com>
2025-10-06 14:04:27 -0400

    R-CODE-01
    R-CODE-01-01

--- src/main/java/org/example/Book.java
+++ src/main/java/org/example/Book.java
@@ -1,4 +1,14 @@
 package org.example;
 
 public class Book {
+    public String title;
+    public String author;
+    public String status;
+
+    public Book(String title, String author){
+        this.title = title;
+        this.author = author;
+        this.status = "AVAILABLE";
+    }
+
 }
--- src/main/java/org/example/LibrarySystem.java
+++ src/main/java/org/example/LibrarySystem.java
@@ -1,12 +1,54 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class LibrarySystem {
+    ArrayList<Book> books = new ArrayList<Book>();
 
-    public void initializeLibrary(){
-
+    public void initializeLibrary() {
+        Book newBook = new Book("Don Quixote", "Miguel de Cervantes");
+        books.add(newBook);
+        newBook = new Book("The Great Gatsby", "F. Scott Fitzgerald");
+        books.add(newBook);
+        newBook = new Book("The Catcher in the Rye", "J. D. Salinger");
+        books.add(newBook);
+        newBook = new Book("Moby Dick", "Herman Melville");
+        books.add(newBook);
+        newBook = new Book("Pride and Prejudice", "Jane Austen");
+        books.add(newBook);
+        newBook = new Book("Dune", "Frank Herbert");
+        books.add(newBook);
+        newBook = new Book("The Lord of the Rings", "J. R. R. Tolkien");
+        books.add(newBook);
+        newBook = new Book("To Kill a Mockingbird", "Harper Lee");
+        books.add(newBook);
+        newBook = new Book("The Trial", "Franz Kafka");
+        books.add(newBook);
+        newBook = new Book("Journey to the West", "Wu Cheng En");
+        books.add(newBook);
+        newBook = new Book("Adventures of Huckleberry Finn", "Mark Twain");
+        books.add(newBook);
+        newBook = new Book("The Odyssey", "Homer");
+        books.add(newBook);
+        newBook = new Book("1984", "George Orwell");
+        books.add(newBook);
+        newBook = new Book("The Divine Comedy", "Dante Alighieri");
+        books.add(newBook);
+        newBook = new Book("The Little Prince", "Antoine de Saint-Exupéry");
+        books.add(newBook);
+        newBook = new Book("The Alchemist", "Paulo Coelho");
+        books.add(newBook);
+        newBook = new Book("Frankenstein", "Mary Shelley");
+        books.add(newBook);
+        newBook = new Book("Les Misérables", "Victor Hugo");
+        books.add(newBook);
+        newBook = new Book("Dracula", "Bram Stoker");
+        books.add(newBook);
+        newBook = new Book("A Christmas Carol", "Charles Dickens");
+        books.add(newBook);
     }
 
-    public int getNumBooks(){
-        return 0;
+    public int getNumBooks() {
+        return books.size();
     }
 }
