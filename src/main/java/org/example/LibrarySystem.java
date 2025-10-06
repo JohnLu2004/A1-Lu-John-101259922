b952e25b2fcabee3625a572f4b51989e9e5d9c44
JohnLu2004 <john.lu10212004@gmail.com>
2025-10-06 13:33:51 -0400

    R-TEST-01
    RESP_01_test_01

--- src/main/java/org/example/LibrarySystem.java
+++ src/main/java/org/example/LibrarySystem.java
@@ -1,4 +1,12 @@
 package org.example;
 
 public class LibrarySystem {
+
+    public void initializeLibrary(){
+
+    }
+
+    public int getNumBooks(){
+        return 0;
+    }
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -3,5 +3,18 @@ package org.example;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MainTest {
+    @Test
+    @DisplayName("Initial Size of the library catalogue is 20")
+    void RESP_01_test_01(){
+        LibrarySystem librarySystem = new LibrarySystem();
+
+        librarySystem.initializeLibrary();
+
+        int numBooks = librarySystem.getNumBooks();
+
+        assertEquals(20, numBooks);
+    }
 }
